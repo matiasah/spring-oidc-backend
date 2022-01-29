20cbb35c85e55b2dc6a10ba93b84b44043a05beb
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-01-29 20:18:58 -0300

    Change login path

--- frontend/src/app/modules/authentication/authentication-routing.module.ts
+++ frontend/src/app/modules/authentication/authentication-routing.module.ts
@@ -5,6 +5,11 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
     {
         path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
         component: LoginComponent
     }
 ];
