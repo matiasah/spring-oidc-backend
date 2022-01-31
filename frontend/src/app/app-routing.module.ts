d2c79dfb5f90323c142c32e88b0ab338592d4bb6
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-01-30 22:56:28 -0300

    Implement guards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './guards/authentication.guard';
+import { SystemGuard } from './guards/system.guard';
 
 const routes: Routes = [
     {
         path: '',
-        loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+        loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule),
+        canActivate: [AuthenticationGuard]
     },
     {
         path: 'sys',
-        loadChildren: () => import('./modules/system/system.module').then(m => m.SystemModule)
+        loadChildren: () => import('./modules/system/system.module').then(m => m.SystemModule),
+        canActivate: [SystemGuard]
     }
 ];
 
