924e01e885211f8e2d8cb6d019af7b76716978cb
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-01-31 00:00:20 -0300

    Add bot icon

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
@@ -12,7 +14,9 @@ export class AppComponent {
     public loading = true;
 
     public constructor(
-        private router: Router
+        private router: Router,
+        private iconRegistry: MatIconRegistry,
+        private domSanitizer: DomSanitizer
     ) {
 
         // Subscribe to router events
@@ -33,6 +37,9 @@ export class AppComponent {
                 }
             }
         );
+
+        // Add 'bot' icon
+        this.iconRegistry.addSvgIcon('bot', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/img/bot.svg'));
 
     }
 
