430daeeb6bcadd6e6a514e17a9a765abd847d294
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-01 22:50:14 -0300

    Integrate QueryDSL

--- backend/src/main/java/oidc/management/repository/ServiceAccountRepository.java
+++ backend/src/main/java/oidc/management/repository/ServiceAccountRepository.java
@@ -1,8 +1,14 @@
 package oidc.management.repository;
 
 import java.util.Optional;
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import oidc.management.model.ServiceAccount;
+import oidc.management.model.QServiceAccount;
 
 /**
  * OIDC Service Account Repository.
@@ -12,11 +18,18 @@ import oidc.management.model.ServiceAccount;
  * @see MongoRepository
  * @see ServiceAccount
  */
-public interface ServiceAccountRepository extends MongoRepository<ServiceAccount, String> {
+public interface ServiceAccountRepository extends MongoRepository<ServiceAccount, String>, QuerydslPredicateExecutor<ServiceAccount>, QuerydslBinderCustomizer<QServiceAccount> {
 
     /**
      * Finds a client by client id.
      */
     public Optional<ServiceAccount> findByClientId(String clientId);
+
+    @Override
+    public default void customize(QuerydslBindings bindings, QServiceAccount qModel) {
+        bindings.bind(Long.class).first((NumberPath<Long> path, Long value) -> path.eq(value));
+        bindings.bind(Integer.class).first((NumberPath<Integer> path, Integer value) -> path.eq(value));
+        bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+    }
     
 }
--- backend/src/main/java/oidc/management/repository/UserAccountRepository.java
+++ backend/src/main/java/oidc/management/repository/UserAccountRepository.java
@@ -1,8 +1,14 @@
 package oidc.management.repository;
 
 import java.util.Optional;
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import oidc.management.model.UserAccount;
+import oidc.management.model.QUserAccount;
 
 /**
  * @author Matías Hermosilla
@@ -12,8 +18,16 @@ import oidc.management.model.UserAccount;
  * @see UserAccountRepository
  * @see UserAccountRepository#findByUsername(String)
  */
-public interface UserAccountRepository extends MongoRepository<UserAccount, String> {
+public interface UserAccountRepository extends MongoRepository<UserAccount, String>, QuerydslPredicateExecutor<UserAccount>, QuerydslBinderCustomizer<QUserAccount> {
 
     public Optional<UserAccount> findByUsername(String username);
+
+    @Override
+    public default void customize(QuerydslBindings bindings, QUserAccount qModel) {
+        bindings.bind(Long.class).first((NumberPath<Long> path, Long value) -> path.eq(value));
+        bindings.bind(Integer.class).first((NumberPath<Integer> path, Integer value) -> path.eq(value));
+        bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+        bindings.excluding(QUserAccount.userAccount.password);
+    }
     
 }
