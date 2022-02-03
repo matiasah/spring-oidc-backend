275f89943aad0179aed0a09e15993824508bd3f0
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-02 22:56:51 -0300

    Create Authority model

---
+++ backend/src/main/java/oidc/management/model/Authority.java
@@ -0,0 +1,37 @@
+package oidc.management.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import oidc.management.repository.AuthorityRepository;
+
+/**
+ * Spring Security Authority model.
+ * 
+ * @author Matías Hermosilla
+ * @since 02-02-2022
+ * @see GrantedAuthority
+ * @see AuthorityRepository
+ */
+@Document(collection = "authorities")
+@Data
+@EqualsAndHashCode(of = "id")
+public class Authority implements GrantedAuthority {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String description;
+    
+    
+    @Override
+    public String getAuthority() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ backend/src/main/java/oidc/management/repository/AuthorityRepository.java
@@ -0,0 +1,33 @@
+package oidc.management.repository;
+
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.core.types.dsl.StringPath;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import oidc.management.model.Authority;
+import oidc.management.model.QAuthority;
+
+/**
+ * Spring Security Authority Repository.
+ * 
+ * @author Matías Hermosilla
+ * @since 02-02-2022
+ * @see MongoRepository
+ * @see Authority
+ * @see QAuthority
+ * @see QuerydslPredicateExecutor
+ * @see QuerydslBinderCustomizer
+ * @see QuerydslBindings
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String>, QuerydslPredicateExecutor<Authority>, QuerydslBinderCustomizer<QAuthority> {
+    
+    @Override
+    public default void customize(QuerydslBindings bindings, QAuthority qModel) {
+        bindings.bind(Long.class).first((NumberPath<Long> path, Long value) -> path.eq(value));
+        bindings.bind(Integer.class).first((NumberPath<Integer> path, Integer value) -> path.eq(value));
+        bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+    }
+
+}
