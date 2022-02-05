e9d268ff1ee280cd9b25d3d114332d393be55cd5
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-05 14:12:48 -0300

    Add UserAccount properties

--- frontend/src/app/interfaces/user-account.ts
+++ frontend/src/app/interfaces/user-account.ts
@@ -1,3 +1,12 @@
+import { Authority } from "./authority";
+
 export interface UserAccount {
     id: string;
+    authorities: Authority[];
+    password: string;
+    username: string;
+    accountNonExpired: boolean;
+    accountNonLocked: boolean;
+    credentialsNonExpired: boolean;
+    enabled: boolean;
 }
