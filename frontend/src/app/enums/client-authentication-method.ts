7dab375780f4cb552534bec1f83111cd6f17e4b9
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-05 14:27:19 -0300

    Create client-authentication-method.ts

---
+++ frontend/src/app/enums/client-authentication-method.ts
@@ -0,0 +1,9 @@
+export enum ClientAuthenticationMethod {
+    BASIC = "basic",
+    CLIENT_SECRET_BASIC = "client_secret_basic",
+    POST = "post",
+    CLIENT_SECRET_POST = "client_secret_post",
+    CLIENT_SECRET_JWT = "client_secret_jwt",
+    PRIVATE_KEY_JWT = "private_key_jwt",
+    NONE = "none",
+}
