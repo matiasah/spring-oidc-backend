695e59be6c71d41da9adeb21e84906e9731f73a3
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-05 14:25:58 -0300

    Create authorization-grant-type.ts

---
+++ frontend/src/app/enums/authorization-grant-type.ts
@@ -0,0 +1,8 @@
+export enum AuthorizationGrantType {
+    AUTHORIZATION_CODE = "authorization_code",
+    IMPLICIT = "implicit",
+    REFRESH_TOKEN = "refresh_token",
+    CLIENT_CREDENTIALS = "client_credentials",
+    PASSWORD = "password",
+    JWT_BEARER = "urn:ietf:params:oauth:grant-type:jwt-bearer",
+}
