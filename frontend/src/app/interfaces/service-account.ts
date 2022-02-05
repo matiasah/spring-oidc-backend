45b995e5276b7dcb4fde13b8c304b62605f5ba72
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-05 14:27:35 -0300

    Update service-account.ts

--- frontend/src/app/interfaces/service-account.ts
+++ frontend/src/app/interfaces/service-account.ts
@@ -1,2 +1,21 @@
+import { AuthorizationGrantType } from "../enums/authorization-grant-type";
+import { ClientAuthenticationMethod } from "../enums/client-authentication-method";
+import { Authority } from "./authority";
+import { ClientSettings } from "./client-settings";
+import { TokenSettings } from "./token-settings";
+
 export interface ServiceAccount {
+    id: string;
+    clientId: string;
+    clientIdIssuedAt: string;
+    clientSecret: string;
+    clientSecretExpiresAt: string;
+    clientName: string;
+    clientAuthenticationMethods: ClientAuthenticationMethod[];
+    authorizationGrantTypes: AuthorizationGrantType[];
+    redirectUris: string[];
+    scopes: string[];
+    clientSettings: ClientSettings;
+    tokenSettings: TokenSettings;
+    authorities: Authority[];
 }
