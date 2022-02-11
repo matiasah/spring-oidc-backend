bcdee6b2cb1ac94b6ef63700221d6e276d055967
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-10 21:20:12 -0300

    Set default values for account expired and locked

--- backend/src/main/java/oidc/management/model/UserAccount.java
+++ backend/src/main/java/oidc/management/model/UserAccount.java
@@ -35,9 +35,9 @@ public class UserAccount implements UserDetails {
     @Setter(onMethod_ = {@JsonSetter})
     private String password;
     private String username;
-    private boolean accountNonExpired;
-    private boolean accountNonLocked;
-    private boolean credentialsNonExpired;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
     private boolean enabled;
 
     private String firstName;
