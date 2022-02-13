85e28d40715ef7f2d54396ff19f3968646937a6b
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-02-13 14:21:07 -0300

    Create ScopeRepository.java

---
+++ backend/src/main/java/oidc/management/repository/ScopeRepository.java
@@ -0,0 +1,22 @@
+package oidc.management.repository;
+
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.core.types.dsl.StringPath;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import oidc.management.model.QScope;
+import oidc.management.model.Scope;
+
+public interface ScopeRepository extends MongoRepository<Scope, String>, QuerydslPredicateExecutor<Scope>, QuerydslBinderCustomizer<QScope> {
+    
+    @Override
+    public default void customize(QuerydslBindings bindings, QScope qModel) {
+        bindings.bind(qModel.id).first((path, value) -> path.eq(value));
+        bindings.bind(Long.class).first((NumberPath<Long> path, Long value) -> path.eq(value));
+        bindings.bind(Integer.class).first((NumberPath<Integer> path, Integer value) -> path.eq(value));
+        bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+    }
+
+}
