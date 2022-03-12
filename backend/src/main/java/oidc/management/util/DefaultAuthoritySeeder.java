30ed3b3e8c3d80c61804fd6e06bea6f7d9f6889b
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-03-11 22:34:58 -0300

    Create DefaultAuthoritySeeder.java

---
+++ backend/src/main/java/oidc/management/util/DefaultAuthoritySeeder.java
@@ -0,0 +1,68 @@
+package oidc.management.util;
+
+import java.util.Optional;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import lombok.extern.java.Log;
+import oidc.management.model.Authority;
+import oidc.management.properties.DefaultAuthorityProperties;
+import oidc.management.repository.AuthorityRepository;
+
+/**
+ * Creates default authorities.
+ * 
+ * @author Matias Hermosilla
+ * @since 20-02-2022
+ * @see DefaultUserAccountProperties
+ */
+@Log
+@ConditionalOnProperty(prefix = "oidc.management.authority", name = "enabled", havingValue = "true")
+@ConditionalOnBean(DefaultAuthorityProperties.class)
+public class DefaultAuthoritySeeder {
+
+    @Autowired
+    private DefaultAuthorityProperties defaultAuthorityProperties;
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    /**
+     * Creates a default user account.
+     */
+    @PostConstruct
+    public void seed() {
+
+        
+
+    }
+
+    /**
+     * Creates a default authority.
+     * 
+     * @param name The authority name.
+     * @param description The authority description.
+     */
+    public void createIfNotExists(String name, String description) {
+
+        // Find if the authority already exists
+        Optional<Authority> optAuthority = authorityRepository.findByName(name);
+
+        // If the authority does not exist, create it
+        if (!optAuthority.isPresent()) {
+
+            // Create the authority object
+            Authority authority = Authority.builder()
+                    .name(name)
+                    .description(description)
+                    .build();
+
+            // Save the authority
+            this.authorityRepository.save(authority);
+
+        }
+
+    }
+    
+}
