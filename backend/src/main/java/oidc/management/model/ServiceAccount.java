a54746dcfcc4bd06d15f3cf73df45160d5fae4fe
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-03-12 21:15:43 -0300

    Adjust ServiceAccount builder

--- backend/src/main/java/oidc/management/model/ServiceAccount.java
+++ backend/src/main/java/oidc/management/model/ServiceAccount.java
@@ -1,5 +1,6 @@
 package oidc.management.model;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +9,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import lombok.AllArgsConstructor;
@@ -55,16 +58,95 @@ public class ServiceAccount {
     private Set<AuthorizationGrantType> authorizationGrantTypes;
     private Set<String> redirectUris;
     private Set<Scope> scopes;
-    private ClientSettings clientSettings;
-    private TokenSettings tokenSettings;
+
+    /**
+     * OIDC Client settings.
+     */
+    private boolean requireAuthenticationConsent = false;
+    private boolean requireProofKey = false;
+
+    /**
+     * JWT Token settings
+     */
+    private Duration accessTokenTimeToLive;
+    private SignatureAlgorithm idTokenSignatureAlgorithm;
+    private Duration refreshTokenTimeToLive;
+    private boolean reuseRefreshTokens = true;
+
+    /**
+     * Authorities
+     */
     private List<Authority> authorities;
 
-    public void setClientSettings(Map<String, Object> clientSettings) {
-        this.clientSettings = ClientSettings.withSettings(clientSettings).build();
+    @Transient
+    public ClientSettings getClientSettings() {
+        return ClientSettings.builder()
+                .requireAuthorizationConsent(this.requireAuthenticationConsent)
+                .requireProofKey(this.requireProofKey)
+                .build();
     }
 
-    public void setTokenSettings(Map<String, Object> tokenSettings) {
-        this.tokenSettings = TokenSettings.withSettings(tokenSettings).build();
+    @Transient
+    public void setClientSettings(ClientSettings clientSettings) {
+        this.setRequireAuthenticationConsent(clientSettings.isRequireAuthorizationConsent());
+        this.setRequireProofKey(clientSettings.isRequireProofKey());
+    }
+
+    @Transient
+    public TokenSettings getTokenSettings() {
+        return TokenSettings.builder()
+                .accessTokenTimeToLive(this.getAccessTokenTimeToLive())
+                .idTokenSignatureAlgorithm(this.getIdTokenSignatureAlgorithm())
+                .refreshTokenTimeToLive(this.getRefreshTokenTimeToLive())
+                .reuseRefreshTokens(this.isReuseRefreshTokens())
+                .build();
+    }
+
+    @Transient
+    public void setTokenSettings(TokenSettings tokenSettings) {
+        this.setAccessTokenTimeToLive(tokenSettings.getAccessTokenTimeToLive());
+        this.setIdTokenSignatureAlgorithm(tokenSettings.getIdTokenSignatureAlgorithm());
+        this.setRefreshTokenTimeToLive(tokenSettings.getRefreshTokenTimeToLive());
+        this.setReuseRefreshTokens(tokenSettings.isReuseRefreshTokens());
+    }
+
+    /**
+     * Service Account builder
+     */
+    public static class ServiceAccountBuilder {
+
+        /**
+         * Sets client settings
+         *
+         * @param clientSettings Client settings
+         * @return Service Account Builder
+         */
+        public ServiceAccountBuilder clientSettings(ClientSettings clientSettings) {
+            // Set client settings
+            this.requireAuthenticationConsent(clientSettings.isRequireAuthorizationConsent());
+            this.requireProofKey(clientSettings.isRequireProofKey());
+
+            // Return the builder
+            return this;
+        }
+
+        /**
+         * Sets token settings
+         *
+         * @param tokenSettings Token settings
+         * @return Service Account Builder
+         */
+        public ServiceAccountBuilder tokenSettings(TokenSettings tokenSettings) {
+            // Set token settings
+            this.accessTokenTimeToLive(tokenSettings.getAccessTokenTimeToLive());
+            this.idTokenSignatureAlgorithm(tokenSettings.getIdTokenSignatureAlgorithm());
+            this.refreshTokenTimeToLive(tokenSettings.getRefreshTokenTimeToLive());
+            this.reuseRefreshTokens(tokenSettings.isReuseRefreshTokens());
+
+            // Return the builder
+            return this;
+        }
+
     }
 
 }
