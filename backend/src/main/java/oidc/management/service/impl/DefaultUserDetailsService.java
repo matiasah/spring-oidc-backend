614028aa1de108476668a9fa552633a08ca0bc45
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-03-27 23:07:12 -0300

    Remove temporal user

--- backend/src/main/java/oidc/management/service/impl/DefaultUserDetailsService.java
+++ backend/src/main/java/oidc/management/service/impl/DefaultUserDetailsService.java
@@ -34,21 +34,6 @@ public class DefaultUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // Temporal user
-        if (username.equals("admin")) {
-
-            // Return user
-            return User.builder()
-                .username(username)
-                .password(passwordEncoder.encode("admin"))
-                .roles("ADMIN")
-                .accountExpired(false)
-                .accountLocked(false)
-                .credentialsExpired(false)
-                .disabled(false)
-                .build();
-        }
-
         // Find user by username
         Optional<UserAccount> userHolder = userAccountRepository.findByUsername(username);
 
