d95f4f9e9536e80d2637b3db8a8743086dde538b
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-03-29 00:03:12 -0300

    Remove QueryDSL from UserAccountRepository

--- backend/src/main/java/oidc/management/repository/UserAccountRepository.java
+++ backend/src/main/java/oidc/management/repository/UserAccountRepository.java
@@ -1,16 +1,10 @@
 package oidc.management.repository;
 
 import java.util.Optional;
-import com.querydsl.core.types.dsl.NumberPath;
-import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 import oidc.management.model.UserAccount;
-import oidc.management.model.QUserAccount;
 
 /**
  * @author Matías Hermosilla
@@ -20,18 +14,15 @@ import oidc.management.model.QUserAccount;
  * @see oidc.management.service.UserAccountService
  * @see oidc.management.service.UserAccountService#findByUsername(String)
  */
-public interface UserAccountRepository extends MongoRepository<UserAccount, String>, QuerydslPredicateExecutor<UserAccount>, QuerydslBinderCustomizer<QUserAccount> {
+public interface UserAccountRepository extends MongoRepository<UserAccount, String> {
 
+    /**
+     * Finds a user by its username.
+     *
+     * @param username The username.
+     * @return The user.
+     */
     public Optional<UserAccount> findByUsername(String username);
-
-    @Override
-    public default void customize(QuerydslBindings bindings, QUserAccount qModel) {
-        bindings.bind(qModel.id).first((path, value) -> path.eq(value));
-        bindings.bind(Long.class).first((NumberPath<Long> path, Long value) -> path.eq(value));
-        bindings.bind(Integer.class).first((NumberPath<Integer> path, Integer value) -> path.eq(value));
-        bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
-        bindings.excluding(QUserAccount.userAccount.password);
-    }
 
     /**
      * Finds users whose alias contains the given search term.
