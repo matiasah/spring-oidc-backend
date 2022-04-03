108968b93b4a29c0cc6da149ce8bcae11b6de14a
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-04-03 17:53:22 -0400

    Enable Jwk update schedule

--- backend/src/main/java/oidc/management/jwk/source/strategy/ScheduledJWKSourceStrategy.java
+++ backend/src/main/java/oidc/management/jwk/source/strategy/ScheduledJWKSourceStrategy.java
@@ -4,7 +4,9 @@ import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSelector;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import lombok.extern.log4j.Log4j2;
 import oidc.management.jwk.JwkProvider;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
 
@@ -14,6 +16,7 @@ import java.util.List;
  * @author Matías Hermosilla
  * @since 03-04-2022
  */
+@Log4j2
 public class ScheduledJWKSourceStrategy implements JWKSource<SecurityContext> {
 
     /**
@@ -42,6 +45,18 @@ public class ScheduledJWKSourceStrategy implements JWKSource<SecurityContext> {
     public List<JWK> get(final JWKSelector jwkSelector, final SecurityContext context) {
         // Return list of JWKs
         return jwks;
+    }
+
+    @Scheduled(cron = "${oidc.management.jwk.schedule}")
+    public void updateJwks() {
+        // Log the update
+        log.info("Updating JWKs");
+
+        // Update the list of JWKs
+        jwks = jwkProvider.getJwks();
+
+        // Log
+        log.info("Updated JWKs");
     }
 
 }
