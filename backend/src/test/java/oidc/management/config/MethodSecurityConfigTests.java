f7902f7aeec9701111e6cd52aa0fc888db694a00
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 18:20:41 -0400

    Create MethodSecurityConfigTests.java

---
+++ backend/src/test/java/oidc/management/config/MethodSecurityConfigTests.java
@@ -0,0 +1,16 @@
+package oidc.management.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {
+        MethodSecurityConfig.class
+})
+public class MethodSecurityConfigTests {
+
+    @Test
+    public void testContext() {
+
+    }
+
+}
