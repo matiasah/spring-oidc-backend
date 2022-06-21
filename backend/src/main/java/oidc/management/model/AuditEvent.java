188dcd025a6ac3f41d2e89027e8decbdadbb7a56
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 17:45:11 -0400

    Create AuditEvent.java

---
+++ backend/src/main/java/oidc/management/model/AuditEvent.java
@@ -0,0 +1,35 @@
+package oidc.management.model;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Entity for auditing authentication events
+ *
+ * @author Matías Hermosilla
+ * @since 02-05-2022
+ */
+public interface AuditEvent {
+
+    public String getId();
+
+    public void setId(String id);
+
+    public String getDescription();
+
+    public void setDescription(String description);
+
+    public ZonedDateTime getCreatedAt();
+
+    public void setCreatedAt(ZonedDateTime createdAt);
+
+    public interface AuditEventBuilder {
+
+        public AuditEventBuilder id(String id);
+
+        public AuditEventBuilder description(String description);
+
+        public AuditEvent build();
+
+    }
+
+}
