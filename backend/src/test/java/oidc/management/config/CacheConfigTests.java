393e7777702fd3fa4ca2dff14fdc7ba80b75a44d
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 18:07:04 -0400

    Create CacheConfigTests.java

---
+++ backend/src/test/java/oidc/management/config/CacheConfigTests.java
@@ -0,0 +1,16 @@
+package oidc.management.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {
+        CacheConfig.class
+})
+public class CacheConfigTests {
+
+    @Test
+    public void testContext() {
+
+    }
+
+}
