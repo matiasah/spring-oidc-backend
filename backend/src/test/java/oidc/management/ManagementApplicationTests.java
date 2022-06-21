d8b0e76ec907eb0b8b93bdd3bce4670bcd5352cf
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 17:44:49 -0400

    Update ManagementApplicationTests.java

--- backend/src/test/java/oidc/management/ManagementApplicationTests.java
+++ backend/src/test/java/oidc/management/ManagementApplicationTests.java
@@ -1,13 +1,33 @@
 package oidc.management;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootTest
-class ManagementApplicationTests {
+@ExtendWith(MockitoExtension.class)
+public class ManagementApplicationTests {
+
+	@Mock
+	private ConfigurableApplicationContext configurableApplicationContext;
 
 	@Test
-	void contextLoads() {
+	public void testMain() {
+		// Mock SpringApplication
+		try (MockedStatic<SpringApplication> mock = Mockito.mockStatic(SpringApplication.class)) {
+
+			// Mock run
+			mock.when(() -> SpringApplication.run(Mockito.eq(ManagementApplication.class), Mockito.any(String[].class)))
+					.thenReturn(configurableApplicationContext);
+
+			// Test main
+			ManagementApplication.main(new String[] {});
+		}
 	}
 
 }
