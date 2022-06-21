c25bb5440bc5efb9ff806d2baec72871f2210324
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 18:25:18 -0400

    Create ValidationConfigTests.java

---
+++ backend/src/test/java/oidc/management/config/ValidationConfigTests.java
@@ -0,0 +1,16 @@
+package oidc.management.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {
+        ValidationConfig.class
+})
+public class ValidationConfigTests {
+
+    @Test
+    public void testContext() {
+
+    }
+    
+}
