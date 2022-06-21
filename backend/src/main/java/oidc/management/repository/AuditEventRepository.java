3c3d4b815d8d9a98d8cd808683530760a906e9bb
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 17:45:33 -0400

    Create AuditEventRepository.java

---
+++ backend/src/main/java/oidc/management/repository/AuditEventRepository.java
@@ -0,0 +1,25 @@
+package oidc.management.repository;
+
+import oidc.management.model.AuditEvent;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+/**
+ * Repository for persisting authentication audit events
+ *
+ * @param <T> An implementation of AuditEvent
+ * @author Matías Hermosilla
+ * @since 02-05-2022
+ * @see {@link PagingAndSortingRepository}
+ * @see {@link AuditEvent}
+ */
+@NoRepositoryBean
+public interface AuditEventRepository<T extends AuditEvent> extends PagingAndSortingRepository<T, String> {
+
+    public AuditEvent.AuditEventBuilder entityBuilder();
+
+    public List<T> findAll();
+
+}
