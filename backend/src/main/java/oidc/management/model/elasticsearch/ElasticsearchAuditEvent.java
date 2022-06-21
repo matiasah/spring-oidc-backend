b59273cf7e276cfef0a950e4083cf94d87c60654
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 17:45:30 -0400

    Create ElasticsearchAuditEvent.java

---
+++ backend/src/main/java/oidc/management/model/elasticsearch/ElasticsearchAuditEvent.java
@@ -0,0 +1,33 @@
+package oidc.management.model.elasticsearch;
+
+import lombok.*;
+import oidc.management.model.AuditEvent;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import javax.validation.constraints.NotNull;
+import java.time.ZonedDateTime;
+
+@Builder
+@Document(indexName = "audit_event")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+public class ElasticsearchAuditEvent implements AuditEvent {
+
+    @Id
+    private String id;
+
+    @NotNull
+    private String description;
+
+    @CreatedDate
+    private ZonedDateTime createdAt;
+
+    public static class ElasticsearchAuditEventBuilder implements AuditEventBuilder {
+
+    }
+
+}
