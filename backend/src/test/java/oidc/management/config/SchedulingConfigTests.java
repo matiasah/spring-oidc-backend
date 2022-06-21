eef71e59f4ee6c86c7de14d81ac1346d1e86e262
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 18:22:12 -0400

    Create SchedulingConfigTests.java

---
+++ backend/src/test/java/oidc/management/config/SchedulingConfigTests.java
@@ -0,0 +1,16 @@
+package oidc.management.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {
+        SchedulingConfig.class
+})
+public class SchedulingConfigTests {
+
+    @Test
+    public void testContext() {
+
+    }
+
+}
