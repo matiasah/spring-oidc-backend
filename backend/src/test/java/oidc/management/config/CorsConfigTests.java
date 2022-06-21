ec52b350f4125f081c720f30c9e693e1a2f9c0e3
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 18:08:15 -0400

    Create CorsConfigTests.java

---
+++ backend/src/test/java/oidc/management/config/CorsConfigTests.java
@@ -0,0 +1,16 @@
+package oidc.management.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {
+        CorsConfig.class
+})
+public class CorsConfigTests {
+
+    @Test
+    public void testContext() {
+
+    }
+    
+}
