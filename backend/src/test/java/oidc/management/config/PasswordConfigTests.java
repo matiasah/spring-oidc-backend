2a535b49dca1449ca6a2ce19d67147643fc0f931
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 18:21:24 -0400

    Create PasswordConfigTests.java

---
+++ backend/src/test/java/oidc/management/config/PasswordConfigTests.java
@@ -0,0 +1,16 @@
+package oidc.management.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {
+        PasswordConfig.class
+})
+public class PasswordConfigTests {
+
+    @Test
+    public void testContext() {
+
+    }
+
+}
