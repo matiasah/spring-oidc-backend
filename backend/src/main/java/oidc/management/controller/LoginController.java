7077a0c72cc7c09adb896921990e678f9a138b50
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-06-21 21:54:59 -0400

    Update LoginController.java

--- backend/src/main/java/oidc/management/controller/LoginController.java
+++ backend/src/main/java/oidc/management/controller/LoginController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Login controller
+ *
+ * @author Matías Hermosilla
+ * @since 29-01-2022
+ */
 @Controller
 public class LoginController {
     
