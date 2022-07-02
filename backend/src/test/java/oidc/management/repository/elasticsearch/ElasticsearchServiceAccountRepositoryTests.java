2b0a56324c39bc4582071b961d1b17b16a9b38d5
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-02 17:15:10 -0400

    Create ElasticsearchServiceAccountRepositoryTests.java

---
+++ backend/src/test/java/oidc/management/repository/elasticsearch/ElasticsearchServiceAccountRepositoryTests.java
@@ -0,0 +1,23 @@
+package oidc.management.repository.elasticsearch;
+
+import oidc.management.model.elasticsearch.ElasticsearchScope;
+import oidc.management.model.elasticsearch.ElasticsearchServiceAccount;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ElasticsearchServiceAccountRepositoryTests {
+
+    @Spy
+    private ElasticsearchServiceAccountRepository elasticsearchServiceAccountRepository;
+
+    @Test
+    public void testEntityBuilder() {
+        // Test entityBuilder
+        Assertions.assertEquals(ElasticsearchServiceAccount.builder().getClass(), elasticsearchServiceAccountRepository.entityBuilder().getClass());
+    }
+
+}
