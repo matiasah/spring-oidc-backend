40dec327ebd0e079d0918b608c98628c1d57fcef
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-04 22:44:40 -0400

    Create DefaultUserAccountEncryptionServiceTests.java

---
+++ backend/src/test/java/oidc/management/service/impl/DefaultUserAccountEncryptionServiceTests.java
@@ -0,0 +1,49 @@
+package oidc.management.service.impl;
+
+import oidc.management.model.UserAccount;
+import oidc.management.service.UserAccountEncryptionService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {
+        DefaultUserAccountEncryptionService.class
+})
+public class DefaultUserAccountEncryptionServiceTests {
+
+    @Autowired
+    private UserAccountEncryptionService userAccountEncryptionService;
+
+    @Mock
+    private UserAccount userAccount;
+
+    @Test
+    public void testEncrypt() {
+        
+        // Test encrypt
+        Assertions.assertNotNull(userAccountEncryptionService.encrypt(userAccount));
+
+    }
+
+    @Test
+    public void testDecrypt() {
+
+        // Test decrypt
+        Assertions.assertNotNull(userAccountEncryptionService.decrypt(userAccount));
+
+    }
+
+    @Test
+    public void testGetHashedName() {
+
+        // Test getHashedName
+        final String hashedName = userAccountEncryptionService.getHashedUsername("test");
+
+        // Validate
+        Assertions.assertNotNull(hashedName);
+
+    }
+
+}
