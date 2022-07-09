e3a13236e4a0951ca1b3c922abadb52c503cd22a
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-09 17:50:28 -0400

    Update DefaultScopeSeederTests.java

--- backend/src/test/java/oidc/management/util/DefaultScopeSeederTests.java
+++ backend/src/test/java/oidc/management/util/DefaultScopeSeederTests.java
@@ -1,69 +1,39 @@
 package oidc.management.util;
 
-import oidc.management.model.Scope;
+import oidc.management.properties.DefaultScopeProperties;
 import oidc.management.service.ScopeService;
-import oidc.management.test.TestScope;
-import org.junit.jupiter.api.Assertions;
+import oidc.management.service.impl.DefaultScopeEncryptionService;
+import oidc.management.service.impl.DefaultScopeService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 
-@ExtendWith(MockitoExtension.class)
+@EnableConfigurationProperties
+@Import({
+        DefaultScopeProperties.class,
+        DefaultScopeSeeder.class,
+        DefaultScopeService.class,
+        DefaultScopeEncryptionService.class
+})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DataJpaTest
 public class DefaultScopeSeederTests {
 
     private static final Set<String> DEFAULT_SCOPES_TAGS = Set.of("default");
 
-    @InjectMocks
+    @Autowired
     private DefaultScopeSeeder defaultScopeSeeder;
 
-    @Mock
+    @Autowired
     private ScopeService scopeService;
 
     @Test
-    public void testGetDefaultScopes() {
-
-        // Mock entityBuilder
-        Mockito.when(scopeService.entityBuilder()).thenReturn(TestScope.builder());
-
-        // Test getDefaultScopes
-        List<Scope> scopes = defaultScopeSeeder.getDefaultScopes();
-
-        // Validate scopes
-        Assertions.assertNotNull(scopes);
-
-        // Validate scopes length
-        Assertions.assertNotEquals(0, scopes.size());
-
-        // For every scope
-        for (Scope scope : scopes) {
-
-            // Validate tags
-            Assertions.assertTrue(scope.getTags().containsAll(DEFAULT_SCOPES_TAGS));
-            Assertions.assertTrue(DEFAULT_SCOPES_TAGS.containsAll(scope.getTags()));
-
-        }
-
-    }
-
-    @Test
     public void testSeed() {
-
-        // Spy defaultScopeSeeder
-        DefaultScopeSeeder defaultScopeSeeder = Mockito.spy(this.defaultScopeSeeder);
-
-        // Mock entityBuilder
-        Mockito.when(scopeService.entityBuilder()).thenReturn(TestScope.builder());
-
-        // Do nothing
-        Mockito.doNothing().when(defaultScopeSeeder).createIfNotExists(Mockito.any(TestScope.class));
 
         // Test seed
         defaultScopeSeeder.seed();
@@ -71,36 +41,13 @@ public class DefaultScopeSeederTests {
     }
 
     @Test
-    public void testCreateIfNotExists() {
+    public void testSeedWithExistingScopes() {
 
-        // Create TestScope
-        TestScope scope = TestScope.builder()
-                .name(UUID.randomUUID().toString())
-                .description(UUID.randomUUID().toString())
-                .build();
+        // Seed scopes
+        defaultScopeSeeder.seed();
 
-        // Mock findByName
-        Mockito.when(scopeService.findByName(scope.getName())).thenReturn(Optional.empty());
-
-        // Test createIfNotExists
-        defaultScopeSeeder.createIfNotExists(scope);
-
-    }
-
-    @Test
-    public void testCreateIfNotExistsWithPresent() {
-
-        // Create TestScope
-        TestScope scope = TestScope.builder()
-                .name(UUID.randomUUID().toString())
-                .description(UUID.randomUUID().toString())
-                .build();
-
-        // Mock findByName
-        Mockito.when(scopeService.findByName(scope.getName())).thenReturn(Optional.of(scope));
-
-        // Test createIfNotExists
-        defaultScopeSeeder.createIfNotExists(scope);
+        // Test seed
+        defaultScopeSeeder.seed();
 
     }
 
