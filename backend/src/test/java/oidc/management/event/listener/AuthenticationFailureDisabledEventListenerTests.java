0492b220c24904ef77801895fefb487158410e61
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-10 16:25:05 -0400

    Create AuthenticationFailureDisabledEventListenerTests.java

---
+++ backend/src/test/java/oidc/management/event/listener/AuthenticationFailureDisabledEventListenerTests.java
@@ -0,0 +1,59 @@
+package oidc.management.event.listener;
+
+import oidc.management.model.AuditEvent;
+import oidc.management.service.AuditEventService;
+import oidc.management.test.TestAuditEvent;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.authentication.event.AuthenticationFailureDisabledEvent;
+import org.springframework.security.core.Authentication;
+
+import java.util.UUID;
+
+@SpringBootTest(classes = {
+        AuthenticationFailureDisabledEventListener.class,
+        ApplicationEventPublisher.class
+})
+public class AuthenticationFailureDisabledEventListenerTests {
+
+    @Autowired
+    private AuthenticationFailureDisabledEventListener listener;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @MockBean
+    private AuditEventService auditEventService;
+
+    @Mock
+    private AuthenticationFailureDisabledEvent event;
+
+    @Mock
+    private Authentication authentication;
+
+    @Test
+    public void testOnApplicationEvent() {
+
+        // Mock getAuthentication
+        Mockito.when(event.getAuthentication()).thenReturn(authentication);
+
+        // Mock getPrincipal
+        Mockito.when(authentication.getPrincipal()).thenReturn(UUID.randomUUID().toString());
+
+        // Mock entityBuilder
+        Mockito.when(auditEventService.entityBuilder()).thenReturn(TestAuditEvent.builder());
+
+        // Dispatch event
+        publisher.publishEvent(event);
+
+        // Verify save
+        Mockito.verify(auditEventService).save(Mockito.any(AuditEvent.class));
+
+    }
+
+}
