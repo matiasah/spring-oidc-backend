b1a3a02f3cb599e681569bfb56877ad48f2484e0
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-10 15:47:58 -0400

    Add implementation to AuthenticationSuccessEventListener

--- backend/src/main/java/oidc/management/event/listener/AuthenticationSuccessEventListener.java
+++ backend/src/main/java/oidc/management/event/listener/AuthenticationSuccessEventListener.java
@@ -1,15 +1,34 @@
 package oidc.management.event.listener;
 
+import lombok.extern.log4j.Log4j2;
+import oidc.management.model.AuditEvent;
+import oidc.management.service.AuditEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class AuthenticationSuccessEventListener implements ApplicationListener<AuthenticationSuccessEvent> {
 
+    @Autowired
+    private AuditEventService auditEventService;
+
     @Override
     public void onApplicationEvent(AuthenticationSuccessEvent event) {
-        System.out.println("AuthenticationSuccessEventListener: " + event.getAuthentication().getName());
+
+        // Create AuditEvent
+        AuditEvent auditEvent = this.auditEventService.entityBuilder()
+                .description(String.format("Authentication success for \"%s\"", event.getAuthentication().getName()))
+                .build();
+
+        // Save the audit event
+        auditEventService.save(auditEvent);
+
+        // Log the audit event
+        log.warn(auditEvent);
+
     }
 
 }
