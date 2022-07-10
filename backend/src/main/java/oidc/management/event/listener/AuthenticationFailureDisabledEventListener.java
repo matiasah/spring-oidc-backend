8ab054a94509915848fc8c84baf17300c18b2598
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-10 15:43:04 -0400

    Add implementation to AuthenticationFailureDisabledEventListener

--- backend/src/main/java/oidc/management/event/listener/AuthenticationFailureDisabledEventListener.java
+++ backend/src/main/java/oidc/management/event/listener/AuthenticationFailureDisabledEventListener.java
@@ -1,15 +1,34 @@
 package oidc.management.event.listener;
 
+import lombok.extern.log4j.Log4j2;
+import oidc.management.model.AuditEvent;
+import oidc.management.service.AuditEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureDisabledEvent;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class AuthenticationFailureDisabledEventListener implements ApplicationListener<AuthenticationFailureDisabledEvent> {
 
+    @Autowired
+    private AuditEventService auditEventService;
+
     @Override
     public void onApplicationEvent(AuthenticationFailureDisabledEvent event) {
-        System.out.println("AuthenticationFailureDisabledEventListener.onApplicationEvent()");
+
+        // Create AuditEvent
+        AuditEvent auditEvent = this.auditEventService.entityBuilder()
+                .description(String.format("Authentication failure, authentication disabled for \"%s\"", event.getAuthentication().getPrincipal()))
+                .build();
+
+        // Save the audit event
+        auditEventService.save(auditEvent);
+
+        // Log the audit event
+        log.warn(auditEvent);
+
     }
 
 }
