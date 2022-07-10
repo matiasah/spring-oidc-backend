7000102a1fc694a051ccb0d60ca1e8483a5c86d9
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-10 15:49:01 -0400

    Update BadCredentialsEventListener.java

--- backend/src/main/java/oidc/management/event/listener/BadCredentialsEventListener.java
+++ backend/src/main/java/oidc/management/event/listener/BadCredentialsEventListener.java
@@ -1,15 +1,34 @@
 package oidc.management.event.listener;
 
+import lombok.extern.log4j.Log4j2;
+import oidc.management.model.AuditEvent;
+import oidc.management.service.AuditEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class BadCredentialsEventListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
 
+    @Autowired
+    private AuditEventService auditEventService;
+
     @Override
     public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
-        System.out.println("Bad credentials event");
+
+        // Create AuditEvent
+        AuditEvent auditEvent = this.auditEventService.entityBuilder()
+                .description(String.format("Authentication failure, bad credentials for \"%s\"", event.getAuthentication().getPrincipal()))
+                .build();
+
+        // Save the audit event
+        auditEventService.save(auditEvent);
+
+        // Log the audit event
+        log.warn(auditEvent);
+
     }
 
 }
