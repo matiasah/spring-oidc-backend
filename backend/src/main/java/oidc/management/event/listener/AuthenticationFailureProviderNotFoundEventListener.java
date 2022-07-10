82ea0f711811ccc352b83a9dc123f05b726cc187
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-10 15:45:28 -0400

    Update AuthenticationFailureProviderNotFoundEventListener.java

--- backend/src/main/java/oidc/management/event/listener/AuthenticationFailureProviderNotFoundEventListener.java
+++ backend/src/main/java/oidc/management/event/listener/AuthenticationFailureProviderNotFoundEventListener.java
@@ -1,15 +1,34 @@
 package oidc.management.event.listener;
 
+import lombok.extern.log4j.Log4j2;
+import oidc.management.model.AuditEvent;
+import oidc.management.service.AuditEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureProviderNotFoundEvent;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class AuthenticationFailureProviderNotFoundEventListener implements ApplicationListener<AuthenticationFailureProviderNotFoundEvent> {
 
+    @Autowired
+    private AuditEventService auditEventService;
+
     @Override
     public void onApplicationEvent(AuthenticationFailureProviderNotFoundEvent event) {
-        System.out.println("AuthenticationFailureProviderNotFoundEventListener");
+
+        // Create AuditEvent
+        AuditEvent auditEvent = this.auditEventService.entityBuilder()
+                .description(String.format("Authentication failure, provider not found for \"%s\"", event.getAuthentication().getPrincipal()))
+                .build();
+
+        // Save the audit event
+        auditEventService.save(auditEvent);
+
+        // Log the audit event
+        log.warn(auditEvent);
+
     }
 
 }
