9c5b740a68451b2c9458e42971147868a7f4477b
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-10 15:43:55 -0400

    Add implementation to AuthenticationFailureExpiredEventListener

--- backend/src/main/java/oidc/management/event/listener/AuthenticationFailureExpiredEventListener.java
+++ backend/src/main/java/oidc/management/event/listener/AuthenticationFailureExpiredEventListener.java
@@ -1,14 +1,33 @@
 package oidc.management.event.listener;
 
+import lombok.extern.log4j.Log4j2;
+import oidc.management.model.AuditEvent;
+import oidc.management.service.AuditEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureExpiredEvent;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class AuthenticationFailureExpiredEventListener implements ApplicationListener<AuthenticationFailureExpiredEvent> {
 
+    @Autowired
+    private AuditEventService auditEventService;
+
     @Override
     public void onApplicationEvent(AuthenticationFailureExpiredEvent event) {
+
+        // Create AuditEvent
+        AuditEvent auditEvent = this.auditEventService.entityBuilder()
+                .description(String.format("Authentication failure, authentication expired for \"%s\"", event.getAuthentication().getPrincipal()))
+                .build();
+
+        // Save the audit event
+        auditEventService.save(auditEvent);
+
+        // Log the audit event
+        log.warn(auditEvent);
 
     }
 
