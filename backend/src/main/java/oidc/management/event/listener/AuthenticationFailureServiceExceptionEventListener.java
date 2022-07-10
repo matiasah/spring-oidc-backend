1e7b0eee4ccc09c01c06f64bee82c77e49e58740
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-07-10 15:46:59 -0400

    Add implementation to AuthenticationFailureServiceExceptionEventListener

--- backend/src/main/java/oidc/management/event/listener/AuthenticationFailureServiceExceptionEventListener.java
+++ backend/src/main/java/oidc/management/event/listener/AuthenticationFailureServiceExceptionEventListener.java
@@ -1,15 +1,34 @@
 package oidc.management.event.listener;
 
+import lombok.extern.log4j.Log4j2;
+import oidc.management.model.AuditEvent;
+import oidc.management.service.AuditEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureServiceExceptionEvent;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class AuthenticationFailureServiceExceptionEventListener implements ApplicationListener<AuthenticationFailureServiceExceptionEvent> {
 
+    @Autowired
+    private AuditEventService auditEventService;
+
     @Override
     public void onApplicationEvent(AuthenticationFailureServiceExceptionEvent event) {
-        System.out.println("AuthenticationFailureServiceExceptionEventListener: " + event.getException().getMessage());
+
+        // Create AuditEvent
+        AuditEvent auditEvent = this.auditEventService.entityBuilder()
+                .description(String.format("Authentication failure, exception ocurred \"%s\" for \"%s\"", event.getException().getMessage(), event.getAuthentication().getPrincipal()))
+                .build();
+
+        // Save the audit event
+        auditEventService.save(auditEvent);
+
+        // Log the audit event
+        log.warn(auditEvent);
+
     }
 
 }
