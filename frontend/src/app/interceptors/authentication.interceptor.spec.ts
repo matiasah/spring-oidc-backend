99120da05dddd38f48ae351f5adf36ec8c322b4e
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-10-15 20:25:47 -0300

    Fix some broken tests

--- frontend/src/app/interceptors/authentication.interceptor.spec.ts
+++ frontend/src/app/interceptors/authentication.interceptor.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
-
-import { AuthenticationInterceptor } from './authentication.interceptor';
+import {TestBed} from '@angular/core/testing';
+import {AuthenticationInterceptor} from './authentication.interceptor';
+import {TestLocalStorage} from "../util/test-local-storage.spec";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AuthenticationInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthenticationInterceptor
-      ]
-  }));
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [
+            RouterTestingModule,
+            HttpClientTestingModule
+        ],
+        providers: [
+            AuthenticationInterceptor,
+            {
+                provide: Storage,
+                useValue: new TestLocalStorage()
+            }
+        ]
+    }));
 
-  it('should be created', () => {
-    const interceptor: AuthenticationInterceptor = TestBed.inject(AuthenticationInterceptor);
-    expect(interceptor).toBeTruthy();
-  });
+    it('should be created', () => {
+        const interceptor: AuthenticationInterceptor = TestBed.inject(AuthenticationInterceptor);
+        expect(interceptor).toBeTruthy();
+    });
 });
