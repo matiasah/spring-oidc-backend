e2ac4547fd175bd469eacae0b6cc06138cbdc5d3
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-11-06 18:58:41 -0300

    Increment SystemGuard coverage

--- frontend/src/app/guards/system.guard.spec.ts
+++ frontend/src/app/guards/system.guard.spec.ts
@@ -3,14 +3,36 @@ import {SystemGuard} from './system.guard';
 import {RouterTestingModule} from "@angular/router/testing";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {TestLocalStorage} from "../util/test-local-storage.spec";
+import {of} from "rxjs";
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
+import {Component} from "@angular/core";
+
+@Component({
+    template: ``
+})
+class TestComponent {
+}
 
 describe('SystemGuard', () => {
     let guard: SystemGuard;
+    let authService: AuthService;
+    let router: Router;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [
-                RouterTestingModule,
+                RouterTestingModule.withRoutes([
+                    {
+                        path: 'sys',
+                        component: TestComponent,
+                        canActivate: [SystemGuard]
+                    },
+                    {
+                        path: '',
+                        component: TestComponent
+                    }
+                ]),
                 HttpClientTestingModule
             ],
             providers: [
@@ -25,10 +47,38 @@ describe('SystemGuard', () => {
             ]
         });
         guard = TestBed.inject(SystemGuard);
+        authService = TestBed.inject(AuthService);
+        router = TestBed.inject(Router);
     });
 
     it('should be created', () => {
         expect(guard).toBeTruthy();
+    });
+
+    it('should allow access', async () => {
+
+        // Spy isAuthenticated
+        spyOn(authService, 'isAuthenticated').and.returnValue(of(true));
+
+        // Route to root
+        await router.navigate(['/sys']);
+
+        // Expect route to be "/"
+        expect(router.url).toBe('/sys');
+
+    });
+
+    it('should not allow access', async () => {
+
+        // Spy isAuthenticated
+        spyOn(authService, 'isAuthenticated').and.returnValue(of(false));
+
+        // Route to root
+        await router.navigate(['/sys']);
+
+        // Expect route to be "/sys"
+        expect(router.url).toBe('/');
+
     });
 
 });
