93a972db45953d378e997403fd04f5f08607c869
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-11-06 17:39:07 -0300

    Fix

--- backend/auth-frontend/src/app/app.component.spec.ts
+++ backend/auth-frontend/src/app/app.component.spec.ts
@@ -6,42 +6,44 @@ import {MaterialModule} from "./modules/material/material.module";
 
 describe('AppComponent', () => {
 
-  let router: Router;
+    let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        MaterialModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-    router = TestBed.inject(Router)
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                MaterialModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+        router = TestBed.inject(Router)
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it('should navigate', async () => {
+    it('should navigate', async () => {
 
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
 
-    fixture.detectChanges()
-    await fixture.whenStable()
+        fixture.detectChanges()
+        await fixture.whenStable()
 
-    await router.navigate(['/']);
+        await router.navigate(['/']);
 
-    fixture.detectChanges()
-    await fixture.whenStable()
-    await router.navigate(['/']);
+        fixture.detectChanges()
+        await fixture.whenStable()
+        await router.navigate(['/']);
+        fixture.detectChanges()
+        await fixture.whenStable()
 
-  });
+    });
 
 });
