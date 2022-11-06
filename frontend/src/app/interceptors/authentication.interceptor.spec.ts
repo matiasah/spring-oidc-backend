6d633059407fb1002ec6223066db94ff7716b870
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-11-06 19:10:55 -0300

    Increment coverage on AuthenticationInterceptor

--- frontend/src/app/interceptors/authentication.interceptor.spec.ts
+++ frontend/src/app/interceptors/authentication.interceptor.spec.ts
@@ -2,29 +2,55 @@ import {TestBed} from '@angular/core/testing';
 import {AuthenticationInterceptor} from './authentication.interceptor';
 import {TestLocalStorage} from "../util/test-local-storage.spec";
 import {RouterTestingModule} from "@angular/router/testing";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {AuthService} from "../services/auth.service";
+import {firstValueFrom} from "rxjs";
+import {environment} from "../../environments/environment";
 
 describe('AuthenticationInterceptor', () => {
-    beforeEach(() => TestBed.configureTestingModule({
-        imports: [
-            RouterTestingModule,
-            HttpClientTestingModule
-        ],
-        providers: [
-            AuthenticationInterceptor,
-            {
-                provide: Storage,
-                useValue: new TestLocalStorage()
-            },
-            {
-                provide: Window,
-                useValue: {}
-            }
-        ]
-    }));
 
-    it('should be created', () => {
-        const interceptor: AuthenticationInterceptor = TestBed.inject(AuthenticationInterceptor);
-        expect(interceptor).toBeTruthy();
+    let authService: AuthService;
+    let controller: HttpTestingController;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                HttpClientTestingModule
+            ],
+            providers: [
+                AuthService,
+                {
+                    provide: HTTP_INTERCEPTORS,
+                    useClass: AuthenticationInterceptor,
+                    multi: true
+                },
+                {
+                    provide: Storage,
+                    useValue: new TestLocalStorage()
+                },
+                {
+                    provide: Window,
+                    useValue: {}
+                }
+            ]
+        });
+        authService = TestBed.inject(AuthService);
+        controller = TestBed.inject(HttpTestingController);
     });
+
+    it('should process token request normally', async () => {
+
+        // Test get token
+        firstValueFrom(authService.exchangeCodeForToken('test'));
+
+        // Expect token request
+        controller.expectOne(`${environment.host}/oauth2/token`).flush({});
+
+        // Verify expectations
+        controller.verify()
+
+    });
+
 });
--- frontend/src/app/interceptors/authentication.interceptor.ts
+++ frontend/src/app/interceptors/authentication.interceptor.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor, HttpHeaders
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor, HttpHeaders
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import {AuthService} from "../services/auth.service";
 import {environment} from "../../environments/environment";
 import {switchMap} from "rxjs/operators";
@@ -14,61 +14,62 @@ import {AccessToken} from "../interfaces/access-token";
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  public constructor(
-    private authService: AuthService
-  ) {
-  }
-
-  public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    // If the requested endpoint is for generating an access token,
-    if (request.url.startsWith(`${environment.host}/oauth2/token`)) {
-
-      // Process the request normally
-      return next.handle(request);
+    public constructor(
+        private authService: AuthService
+    ) {
     }
 
-    // If the requested endpoint is for a protected resource,
-    if (request.url.startsWith(environment.host)) {
+    public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-      // If the user is authenticated
-      return this.authService.isAuthenticated()
-        .pipe(switchMap(
-          (isAuthenticated: boolean) => {
+        // If the requested endpoint is for generating an access token,
+        if (request.url.startsWith(`${environment.host}/oauth2/token`)) {
 
-            // If there's no authentication
-            if (!isAuthenticated) {
-
-              // Process the request normally
-              return next.handle(request);
-            }
-
-            // Get authenticated user from localStorage
-            const accessToken: AccessToken | undefined = this.authService.getToken();
-
-            // If there is a token
-            if (accessToken) {
-
-              // Create a new request with the token
-              const headers: HttpHeaders = request.headers.set('Authorization', `Bearer ${accessToken.access_token}`);
-
-              // Copy the original request and replace the headers
-              const newRequest: HttpRequest<any> = request.clone({
-                headers
-              });
-
-              // Send the new request
-              return next.handle(newRequest);
-            }
-
-            // Return request
+            // Process the request normally
             return next.handle(request);
-          }
-        ));
-    }
 
-    // Send the request
-    return next.handle(request);
-  }
+        }
+
+        // If the requested endpoint is for a protected resource,
+        if (request.url.startsWith(environment.host)) {
+
+            // If the user is authenticated
+            return this.authService.isAuthenticated()
+                .pipe(switchMap(
+                    (isAuthenticated: boolean) => {
+
+                        // If there's no authentication
+                        if (!isAuthenticated) {
+
+                            // Process the request normally
+                            return next.handle(request);
+                        }
+
+                        // Get authenticated user from localStorage
+                        const accessToken: AccessToken | undefined = this.authService.getToken();
+
+                        // If there is a token
+                        if (accessToken) {
+
+                            // Create a new request with the token
+                            const headers: HttpHeaders = request.headers.set('Authorization', `Bearer ${accessToken.access_token}`);
+
+                            // Copy the original request and replace the headers
+                            const newRequest: HttpRequest<any> = request.clone({
+                                headers
+                            });
+
+                            // Send the new request
+                            return next.handle(newRequest);
+                        }
+
+                        // Return request
+                        return next.handle(request);
+                    }
+                ));
+        }
+
+        // Send the request
+        return next.handle(request);
+    }
 
 }
