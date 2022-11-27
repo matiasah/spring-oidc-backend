ac963a51033f5e25537d6a4abad32aabbad7f731
Matías Hermosilla <matiasahermosilla@gmail.com>
2022-11-27 20:17:04 -0300

    Update MongoJwkProviderTests.java

--- backend/src/test/java/oidc/management/jwk/provider/MongoJwkProviderTests.java
+++ backend/src/test/java/oidc/management/jwk/provider/MongoJwkProviderTests.java
@@ -46,7 +46,7 @@ public class MongoJwkProviderTests {
 
     @Test
     public void testGetJwks() {
-        
+
         // Generate JWK
         Map<String, Object> key = rsaKeyGenerator.generateRsa().toJSONObject();
 
