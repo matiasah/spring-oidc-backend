f8104ebb44225b6a6f8fb89d6e3b7543007ebaad
Matías Hermosilla <matiasahermosilla@gmail.com>
2023-09-09 15:29:57 -0300

    Migrate to Angular 16

--- frontend/src/app/guards/authentication.guard.ts
+++ frontend/src/app/guards/authentication.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
@@ -13,7 +13,7 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
     providedIn: 'root'
 })
-export class AuthenticationGuard implements CanActivate {
+export class AuthenticationGuard  {
 
     public constructor(
         private authService: AuthService,
--- frontend/src/app/guards/system.guard.ts
+++ frontend/src/app/guards/system.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
@@ -13,7 +13,7 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
     providedIn: 'root'
 })
-export class SystemGuard implements CanActivate {
+export class SystemGuard  {
 
     public constructor(
         private authService: AuthService,
